096b5088ea0bf346e3f22e3037e34cea4a2091f6
202130234 최성현 <hyunii0423@gmail.com>
2025-04-10 12:35:39 +0900

    v1.7 kiosks

--- week06.kiosk.py
+++ week06.kiosk.py
@@ -18,9 +18,10 @@ def order_process(idx):
     amounts[idx] = amounts[idx] + 1
 
 
-menu_texts = ""
-for j in range(len(drinks)):
-    menu_texts = menu_texts + f"{j+1}) {drinks[j]} {prices[j]}원  "
+# menu_texts = ""
+# for j in range(len(drinks)):
+#    menu_texts = menu_texts + f"{j+1}) {drinks[j]} {prices[j]}원  "
+menu_texts = "".join(f"{j+1}) {drinks[j]} {prices[j]}원 " for j in range(len(drinks)))
 menu_texts = menu_texts + f"{len(drinks)+1}) 주문종료 : "
 
 while True:

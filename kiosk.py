1d4ed6e4d983c2837c08feac5d5f3fff13937ed4
202130234 최성현 <hyunii0423@gmail.com>
2025-05-08 11:50:52 +0900

    v2.9 kiosk

--- kiosk.py
+++ kiosk.py
@@ -1,14 +1,14 @@
 drinks = ["아이스 아메리카노", "카페 라떼", "수박 주스", "딸기 주스"]
 prices = [1500, 2500, 4000, 4200]
-
 # drinks = ["아이스 아메리카노"]
 # prices = [1500]
 amounts = [0] * len(drinks)
 total_price = 0
 
 # 할인 적용 정책
-DISCOUNT_THRESHOLD = 10000 # 할인이 적용되는 임계값 (임계값 이상이면 할인 적용)
-DISCOUNT_RATE = 0.1 # 할인율
+DISCOUNT_THRESHOLD = 10000  # 할인이 적용되는 임계값 (임계값 이상이면 할인 적용)
+DISCOUNT_RATE = 0.1  # 할인율
+
 
 def apply_discount(price: int) -> float:
     """
@@ -17,9 +17,27 @@ def apply_discount(price: int) -> float:
     :return: 할인이 적용된 금액 또는 할인이 적용되지 않은 금액
     """
     if price >= DISCOUNT_THRESHOLD:
-        return price * (1 - DISCOUNT_RATE)
-    return  price
+        return  price * (1 - DISCOUNT_RATE)
+    return price
 
+
+def get_ticket_number() -> int:
+    """
+    주문 번호표 처리 기능 함수
+    :return: 번호
+    """
+    try:
+        with open("ticket.txt", "r") as fp:
+            number = int(fp.read())
+    except FileNotFoundError:
+        number = 0
+
+    number = number + 1
+
+    with open("ticket.txt", "w") as fp:
+        fp.write(str(number))
+
+    return number
 
 
 def order_process(idx: int) -> None:
@@ -36,7 +54,7 @@ def order_process(idx: int) -> None:
 
 def display_menu() -> str:
     """
-    음료 선택 메뉴 디스플레이 기능
+    음료 선택 메뉴 디스플레이 함수
     :return: 음료 메뉴 및 주문 종료 문자열
     """
     print("="*30)
@@ -55,7 +73,15 @@ def print_receipt() -> None:
         if amounts[i] > 0:
             print(f"{drinks[i]:^20}{prices[i]:^6}{amounts[i]:^6}{prices[i] * amounts[i]:^6}")
 
-    print(f"총 주문 금액 : {total_price}원")
+    discounted_price = apply_discount(total_price)
+    discount = total_price - discounted_price
+
+    print(f"할인 전 총 주문 금액 : {total_price}원")
+    if discount > 0:
+        print(f"할인 금액 : {discount}원")
+        print(f"할인 적용 후 지불하실 총 금액은 {discounted_price}원 입니다.")
+    else:
+        print(f"할인이 적용되지 않았습니다.\n지불하실 총 금액은 {total_price}원 입니다.")
 
 
 def test() -> None:
